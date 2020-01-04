7024bbaeda05cd545642abc2961387d4800a89a0
akane <corynarigato@gmail.com>
2020-01-04 20:03:18 +0900

    feat api

--- api/Auth.py
+++ api/Auth.py
@@ -1,6 +1,6 @@
 import virty_api as api
 
-api.API_URL = "http://localhost:80"
+api.API_URL = "http://192.168.144.44:80"
 
 api.auth_login("user2","abcxyz")
 api.auth_test()
--- api/DomainList.py
+++ api/DomainList.py
@@ -1,6 +1,6 @@
 import virty_api as api
 
-api.API_URL = "http://localhost:80"
+api.API_URL = "http://192.168.144.44:80"
 
 domains = api.domain_data()
 
