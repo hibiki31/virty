26ded34678b1cf71e0e7f8f97fbc9c2cb3b147a1
hibiki31 <corynarigato@gmail.com>
2020-11-19 22:09:54 +0900

    Add タスク状況のロングポーリング (API)

--- main/task/router.py
+++ main/task/router.py
@@ -8,6 +8,8 @@ from auth.router import get_current_user, CurrentUser
 from .models import *
 from .schemas import *
 
+import asyncio
+
 
 app = APIRouter()
 
@@ -18,6 +20,29 @@ async def get_api_tasks(
         db: Session = Depends(get_db)
     ):
     task = db.query(TaskModel).order_by(desc(TaskModel.post_time)).all()
+    
+    return task
+
+
+@app.get("/api/tasks/{uuid}", tags=["task"], response_model=TaskSelect)
+async def get_api_tasks(
+        current_user: CurrentUser = Depends(get_current_user),
+        db: Session = Depends(get_db),
+        uuid: str = None,
+        polling: bool = False
+    ):
+    while True:
+        try:
+            task: TaskModel = db.query(TaskModel).filter(TaskModel.uuid==uuid).one()
+        except:
+            raise HTTPException(status_code=400, detail="task not fund")
+        if task.status == "finish":
+            break
+        if polling:
+            await asyncio.sleep(0.5)
+            db.commit() # こいつないと、他が加えたDBの変更入らない
+            continue
+        break
     
     return task
 
