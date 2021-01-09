2efb6362b1e34155cfc6a4f02f6523d444cdc052
hibiki31 <corynarigato@gmail.com>
2021-01-10 02:55:19 +0900

    Delete 旧モジュールの削除

--- main/module/ref_ldap.py
+++
@@ -1,8 +0,0 @@
-import ldap3
-
-server = ldap3.Server("ldap://",get_info=ldap3.ALL)
-conn = ldap3.Connection(server,'cn=admin,dc=nodomain','admin')
-print(conn)
-print(conn.bind())
-
-conn.extend.standard.who_am_i()
