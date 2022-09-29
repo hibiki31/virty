e469e4c520380bf5628cce3f6f02d4ec9330f0bf
taso0096 <jin2931121@yahoo.co.jp>
2022-09-29 22:50:31 +0900

    Chore: Add MUI related libraries

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,5 +1,11 @@
 import type { AppProps } from 'next/app';
 
+// Roboto fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
