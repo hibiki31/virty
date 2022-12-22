a8e52442498fc43d5f2b314aec45e90642b1a930
hibiki31 <corynarigato@gmail.com>
2022-12-22 16:10:54 +0000

    Add test

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -1,10 +1,23 @@
 from fastapi.testclient import TestClient
+import json
 
-from auth.router import app
+from main import app
 
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/api/auth/setup")
-    assert response.json() == {"msg": "Hello World"}
+def test_auth():
+    json_open = open('./tests/env.json', 'r')
+    json_load = json.load(json_open)
+    response = client.post(
+        "/api/auth",
+        data={"username": json_load["user"], "password": json_load["password"]},
+    )
+    print(response.json())
+    print(json_load)
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "foobar",
+        "title": "Foo Bar",
+        "description": "The Foo Barters",
+    }
