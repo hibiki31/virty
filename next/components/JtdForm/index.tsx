180a02ec8cf8082ef508e6a71426c44b6c61ecbe
taso0096 <jin2931121@yahoo.co.jp>
2022-12-30 15:57:50 +0900

    Add: JTD form component

---
+++ next/components/JtdForm/index.tsx
@@ -0,0 +1,40 @@
+import { Grid } from '@mui/material';
+import { Schema } from 'jtd';
+import { FC, useEffect } from 'react';
+import { useJtdForm } from '~/store/formState';
+import { MetaData } from '~/lib/jtd/types';
+import { PropertyInput } from './PropertyInput';
+
+type Props = {
+  prefixPropertyName?: string;
+  propertyJtd?: Schema & { metadata?: MetaData };
+  rootJtd: Schema & { metadata?: MetaData };
+  isEditing: boolean;
+};
+
+export const JtdForm: FC<Props> = ({ prefixPropertyName, propertyJtd, rootJtd, isEditing }) => {
+  const { reset } = useJtdForm(rootJtd);
+  const propertiesJtd: Schema = (propertyJtd as any)?.properties || (rootJtd as any).properties;
+
+  useEffect(
+    () => () => {
+      reset();
+    },
+    [reset]
+  );
+
+  return (
+    <Grid container rowSpacing={2}>
+      {Object.entries(propertiesJtd).map(([propertyKey, jtd]) => (
+        <PropertyInput
+          key={propertyKey}
+          prefixPropertyName={prefixPropertyName}
+          propertyKey={propertyKey}
+          propertyJtd={jtd}
+          rootJtd={rootJtd}
+          isEditing={isEditing}
+        />
+      ))}
+    </Grid>
+  );
+};
