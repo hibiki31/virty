26c40fa9fd5b2c7e7b00e7b8b1998e413ca22c4e
taso0096 <jin2931121@yahoo.co.jp>
2023-03-11 16:30:45 +0900

    Add: network page

--- next/components/layouts/NavigationDrawer.tsx
+++ next/components/layouts/NavigationDrawer.tsx
@@ -15,7 +15,7 @@ import {
   Typography,
   useTheme,
 } from '@mui/material';
-import { Home, CubeOutline, CheckboxMultipleMarkedOutline, Server } from 'mdi-material-ui';
+import { Home, CubeOutline, CheckboxMultipleMarkedOutline, Server, Wan } from 'mdi-material-ui';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
 import { FC, useEffect, useState } from 'react';
@@ -39,6 +39,11 @@ const drawerRoutes = [
     title: 'Node',
     path: '/node',
     icon: <Server />,
+  },
+  {
+    title: 'Network',
+    path: '/network',
+    icon: <Wan />,
   },
   {
     title: 'Task',
---
+++ next/pages/network.tsx
@@ -0,0 +1,25 @@
+import { Grid, Typography } from '@mui/material';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { DefaultLayout } from '~/components/layouts/DefaultLayout';
+import { makeRequireLoginProps } from '~/lib/utils/makeGetServerSideProps';
+
+export const getServerSideProps = makeRequireLoginProps();
+
+const Page: NextPage = () => {
+  return (
+    <DefaultLayout>
+      <Head>
+        <title>Virty - Network</title>
+      </Head>
+
+      <Grid container alignItems="center" spacing={2} sx={{ mt: 0, mb: 1 }}>
+        <Grid item>
+          <Typography variant="h4">Network</Typography>
+        </Grid>
+      </Grid>
+    </DefaultLayout>
+  );
+};
+
+export default Page;
