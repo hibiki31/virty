2eb7215edf1ea4740257af540710bd9430be6113
taso0096 <jin2931121@yahoo.co.jp>
2023-03-12 02:13:30 +0900

    Add: AddNetworkPoolDialog component

--- next/pages/network.tsx
+++ next/pages/network.tsx
@@ -1,6 +1,8 @@
 import { Grid, Typography } from '@mui/material';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { OpenDialogButton } from '~/components/buttons/OpenDialogButton';
+import { AddNetworkPoolDialog } from '~/components/dialogs/AddNetworkPoolDialog';
 import { DefaultLayout } from '~/components/layouts/DefaultLayout';
 import { NetworkPoolsTable } from '~/components/tables/NetworkPoolsTable';
 import { NetworksTable } from '~/components/tables/NetworksTable';
@@ -25,6 +27,9 @@ const Page: NextPage = () => {
       <Grid container alignItems="center" spacing={2} sx={{ mt: 0, mb: 1 }}>
         <Grid item>
           <Typography variant="h4">Network Pool</Typography>
+        </Grid>
+        <Grid item>
+          <OpenDialogButton label="Add" DialogComponent={AddNetworkPoolDialog} buttonProps={{ variant: 'contained' }} />
         </Grid>
       </Grid>
       <NetworkPoolsTable />
