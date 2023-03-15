02a314f13fdf9c168706cb9ba48d37d344359a4c
taso0096 <jin2931121@yahoo.co.jp>
2023-03-16 01:04:34 +0900

    Add: AddUserDialog component

--- next/pages/users.tsx
+++ next/pages/users.tsx
@@ -1,6 +1,8 @@
 import { Grid, Typography } from '@mui/material';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { OpenDialogButton } from '~/components/buttons/OpenDialogButton';
+import { AddUserDialog } from '~/components/dialogs/AddUserDialog';
 import { DefaultLayout } from '~/components/layouts/DefaultLayout';
 import { UsersTable } from '~/components/tables/UsersTable';
 import { makeRequireLoginProps } from '~/lib/utils/makeGetServerSideProps';
@@ -17,6 +19,9 @@ const Page: NextPage = () => {
       <Grid container alignItems="center" spacing={2} sx={{ mt: 0, mb: 1 }}>
         <Grid item>
           <Typography variant="h4">Users</Typography>
+        </Grid>
+        <Grid item>
+          <OpenDialogButton label="Add" DialogComponent={AddUserDialog} buttonProps={{ variant: 'contained' }} />
         </Grid>
       </Grid>
 
