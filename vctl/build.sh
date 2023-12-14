db67fa9f38cfc627135e13270d3ae5eec8130ff4
hibiki31 <corynarigato@gmail.com>
2023-12-14 11:28:52 +0000

    Fix nuitka build command

--- vctl/build.sh
+++ vctl/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 docker compose build
-docker compose run ubuntu python3 -m  nuitka --onefile --standalone --follow-imports virty.py
+docker compose run ubuntu python3 -m nuitka \
+    --onefile \
+    --standalone \
+    --include-package=typer \
+    --include-package=tabulate \
+    --follow-imports virty.py
 # python3 -m  nuitka --onefile --follow-imports virty.py
