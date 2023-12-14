a202172f9bb7c77077f95ad3e51cd3e9442ab2b0
hibiki31 <corynarigato@gmail.com>
2023-12-14 15:12:10 +0000

    Fix build script

--- vctl/README.md
+++ vctl/README.md
@@ -7,3 +7,9 @@
 ```
 export PYTHONPATH="/home/akane/virty/vctl:$PYTHONPATH"
 ```
+
+```
+sudo chmod 755 ./main.bin
+sudo cp ./main.bin /usr/local/bin/virty
+virty --install-completion bash
+```
--- vctl/build.sh
+++ vctl/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 docker compose build
-docker compose run ubuntu python3 -m nuitka \
+docker compose up -d
+docker compose exec -it ubuntu pip install /opt/lib
+docker compose exec -it ubuntu python3 -m nuitka \
     --onefile \
     --standalone \
     --include-package=typer \
     --include-package=tabulate \
+    --include-package=virty \
     --follow-imports main.py
