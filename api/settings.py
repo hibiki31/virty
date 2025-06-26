17c6f67f7a7a51144c5a5ca1998f0bfedd3c082f
hibiki31 <corynarigato@gmail.com>
2025-06-27 01:57:57 +0900

    Bump API and package version to 5.1.2 for consistency across the project

--- api/settings.py
+++ api/settings.py
@@ -6,7 +6,7 @@ SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL', 'postgresql://pos
 APP_ROOT = os.getenv('APP_ROOT', str(pathlib.Path('./').resolve()))
 DATA_ROOT = os.getenv('DATA_ROOT', str(pathlib.Path('./data').resolve()))
 IS_DEV = bool(os.getenv('IS_DEV', False))
-API_VERSION = '5.1.1'
+API_VERSION = '5.1.2'
 SECRET_KEY = 'DEV_KEY' if IS_DEV else os.getenv('SECRET_KEY', secrets.token_hex(32))
 
 # Ansible用のパスを指定
