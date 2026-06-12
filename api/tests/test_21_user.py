f490bd81e292216f2f6c54f629ca5218b8c6c380
hibiki31 <corynarigato@gmail.com>
2026-06-12 14:32:43 +0000

    Add user update test and assert delete user response status
    Add a test for updating users by sending POST requests with user data.
    Also, add assertions to verify that delete user requests return status 200,
    ensuring API endpoints behave as expected during user modifications.

--- api/tests/test_21_user.py
+++ api/tests/test_21_user.py
@@ -12,3 +12,14 @@ def test_delete_user(env, client):
     for user in env.users:
         res = client.delete(f'/api/users/{user.username}')
         assert res.status_code == 200
+
+
+def test_update_user(env, client):
+    for user in env.users:
+        req_data = {
+            "username": user.username,
+            "password": user.password,
+            "scopes": 
+        }
+        res = client.post("/api/users", json=req_data)
+        assert res.status_code == 200
